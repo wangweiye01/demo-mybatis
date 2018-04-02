ba0c8dca102d78ed4731ae2a92d1a0b76c8cad50
wangweiye <wwyknight@163.com>
2018-04-02 08:40:34 +0800

    fix

--- data.sql
+++ data.sql
@@ -1,5 +1,37 @@
-INSERT INTO `authority` VALUES (1,'ROLE_USER'),(2,'ROLE_ADMIN');
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `enabled` bit(1) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastpasswordresetdate` datetime NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `created_at` TIMESTAMP DEFAULT current_timestamp,
+  `updated_at` TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户表';
+
+DROP TABLE IF EXISTS `authority`;
+CREATE TABLE `authority` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `created_at` TIMESTAMP DEFAULT current_timestamp,
+  `updated_at` TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='权限表';
+
+DROP TABLE IF EXISTS `user_authority`;
+CREATE TABLE `user_authority` (
+  `user_id` bigint(20) NOT NULL,
+  `authority_id` bigint(20) NOT NULL,
+  `created_at` TIMESTAMP DEFAULT current_timestamp,
+  `updated_at` TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp NULL
+) ENGINE=InnoDB AUTO_INCREMENT=100001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户权限关联表';
+
+INSERT INTO `authority`(id,name) VALUES (1,'ROLE_USER'),(2,'ROLE_ADMIN');
 
 INSERT INTO user(id,username,password,firstname,lastname,email,enabled,lastpasswordresetdate) VALUES (1,'admin','$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi','admin','admin','admin1@admin.com',1,'2017-01-01'),(3,'disabled','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','user','user','disabled@user.com',0,'2017-01-01'),(2,'user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','user','user','enabled@user.com',1,'2017-01-01');
 
-INSERT INTO `user_authority` VALUES (1,1),(1,2),(2,1),(3,1);
+INSERT INTO `user_authority`(user_id, authority_id) VALUES (1,1),(1,2),(2,1),(3,1);
--- src/main/java/com/example/demo/common/utils/BaseEntity.java
+++ src/main/java/com/example/demo/common/utils/BaseEntity.java
@@ -1,25 +1,14 @@
 package com.example.demo.common.utils;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
 public class BaseEntity {
     protected Integer id;
     protected String createdAt;
-    @JsonIgnore
-    protected String deletedAt;
-    @JsonIgnore
+    protected String updatedAt;
     protected Integer isDelete;
     public static final Integer DELETED = 1;
     public static final Integer NO_DELETED = 0;
-
-    public void preInsert() {
-        this.createdAt = "";
-    }
-
-    public void preDelete() {
-        this.deletedAt = "";
-    }
 }
