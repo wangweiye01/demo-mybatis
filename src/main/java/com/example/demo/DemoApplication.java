1d4559758dc22799f99621ebc7e3dbe8febb60ee
wangweiye <wwyknight@163.com>
2018-09-18 14:12:56 +0800

    fix

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -8,6 +9,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ServletComponentScan(value = {"com.example.demo.filter"})
+@EnableEncryptableProperties
 public class DemoApplication {
 
 	public static void main(String[] args) {
