91435e539a650ebc25a9cef58664a0e623f3c217
wangweiye <wwyknight@163.com>
2018-11-07 15:42:51 +0800

    fix

--- src/test/java/com/example/demo/AntPathMatcherTest.java
+++ src/test/java/com/example/demo/AntPathMatcherTest.java
@@ -5,16 +5,35 @@ import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
 
 import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.assertFalse;
 
 public class AntPathMatcherTest {
     @Test
     public void antPathMatcherTest() {
         PathMatcher matcher = new AntPathMatcher();
 
-        String requestPath = "/hellos/1/read";
-        String patternPath = "/hellos/{id}/read";
+        String requestPath1 = "/hellos/1/read";
+        String patternPath1 = "/hellos/{id}/read";
 
-        boolean result = matcher.match(patternPath, requestPath);
+        String requestPath2 = "/hellos/a/b/c/d";
+        String patternPath2 = "/hellos/**";
+
+        String requestPath3 = "/hellos/a/b/c";
+        String patternPath3 = "/hellos/*";
+
+        String requestPath4 = "/a/b/hellos/c";
+        String patternPath4 = "/**/hellos/*";
+
+        boolean result = matcher.match(patternPath1, requestPath1);
+        assertTrue(result);
+
+        result = matcher.match(patternPath2, requestPath2);
+        assertTrue(result);
+
+        result = matcher.match(patternPath3, requestPath3);
+        assertFalse(result);
+
+        result = matcher.match(patternPath4, requestPath4);
         assertTrue(result);
     }
 }
