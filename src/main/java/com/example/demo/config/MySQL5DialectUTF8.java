529f4bc14f4f7a965924c7045d17b0bfd6768456
wangweiye <wwyknight@163.com>
2018-11-09 11:14:42 +0800

    fix

--- src/main/java/com/example/demo/config/MySQL5DialectUTF8.java
+++ src/main/java/com/example/demo/config/MySQL5DialectUTF8.java
@@ -5,6 +5,6 @@ import org.hibernate.dialect.MySQL5InnoDBDialect;
 public class MySQL5DialectUTF8 extends MySQL5InnoDBDialect {
     @Override
     public String getTableTypeString() {
-        return " ENGINE=InnoDB DEFAULT CHARSET=utf8";
+        return " ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
     }
 }
